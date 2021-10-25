44169a9c91c8d3b238c5ee5e9bf3443ecb9e7f53
Samvitgoyal <76777983+Samvitgoyal@users.noreply.github.com>
2021-10-25 19:47:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup() {
+  canvas = createCanvas(300,240);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+}
+
+function draw(){
+
+}
+
+function preload(){
+
+}
+
+
